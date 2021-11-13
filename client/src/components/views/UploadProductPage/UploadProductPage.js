cdf1b163f0029a8a3dadf146cda5446f9c423128
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-11-13 16:22:51 +0900

    Create: 파일 업로드를 위한 Dropzone 생성

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Typography, Button, Form, Input } from 'antd';
+import FileUpload from '../../utils/FileUpload';
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -39,6 +40,7 @@ function UploadProductPage() {
       </div>
 
       <Form>
+        <FileUpload />
         <br />
         <br />
         <label>이름</label>
